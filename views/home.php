67804080ba748a8bce82613be57ba4ddcc32c192
andrean <andrean@gmail.com>
2020-09-03 05:31:09 +0700

    Data baru

--- views/home.php
+++ views/home.php
@@ -36,9 +36,9 @@
   <hr>
 <!--Content Web-->
 
-<div style="margin-left: 30px; margin-top: 30px;">
+<div style="padding-left: 100px; padding-top: 30px;">
 	<div class="row">
-		<div class="card" style="width: 18rem;">
+		<div class="card" style="width: 18rem; margin-left: 30px;">
 		  <img src="assets/img/tmp2.jpg" class="card-img-top" alt="...">
 		  <div class="card-body">
 		    <h5 class="card-title">Card title</h5>
@@ -46,5 +46,13 @@
 		    <a href="#" class="btn btn-primary">Go somewhere</a>
 		  </div>
 		</div>
+    <div class="card" style="width: 18rem; margin-left: 30px;">
+      <img src="assets/img/tmp2.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
 	</div>
 </div>
