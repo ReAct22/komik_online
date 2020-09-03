f7a121a40529d8ec0072d15bc122263a08bd3d74
andrean <andrean@gmail.com>
2020-09-03 23:16:45 +0700

    Data update

--- admin/index.php
+++ admin/index.php
@@ -58,13 +58,13 @@
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
           <i class="fas fa-fw fa-cog"></i>
-          <span>Components</span>
+          <span>Page</span>
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
+            <a class="collapse-item" href="?page=post">Post</a>
+            <a class="collapse-item" href="?page=page">Halaman</a>
           </div>
         </div>
       </li>
@@ -233,7 +233,13 @@
           <!-- Content Row -->
           <div class="row">
 
-            
+            <?php 
+              $page = @$_GET['page'];
+              if($page == ""){
+                include "views/beranda.php";
+              }
+            ?>
+
           <!-- Content Row -->
 
           <div class="row">
